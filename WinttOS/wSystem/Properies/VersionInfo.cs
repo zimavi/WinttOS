5af682b769cad583e748f6ea71a9b4253b1df523
Anton Bondarshuk <bar05022009@gmail.com>
2024-04-30 21:37:41 +0300

    Pre-merge fixes

--- WinttOS/wSystem/Properies/VersionInfo.cs
+++ WinttOS/wSystem/Properies/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace WinttOS.wSystem
 {
     public sealed class VersionInfo
     {
-        public static readonly string revision = "300420242057";
+        public static readonly string revision = "300420242136";
     }
 }
