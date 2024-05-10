c2c3eb1483645e1872036673f3e9f54aac0734eb
Anton Bondarshuk <bar05022009@gmail.com>
2024-05-10 11:32:29 +0300

    feat: Make kernel panic more linux like

--- WinttOS/Core/Utils/Kernel/KernelPanic.cs
+++ WinttOS/Core/Utils/Kernel/KernelPanic.cs
@@ -1,18 +1,13 @@
 ﻿using Cosmos.System;
+using Cosmos.System.Coroutines;
 using Cosmos.System.Graphics;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using WinttOS.Core.Utils.Debugging;
 using WinttOS.Core.Utils.Sys;
 
 namespace WinttOS.Core.Utils.Kernel
 {
     internal sealed class KernelPanic
     {
-        private static Canvas _canvas;
         private static WinttStatus _status;
-        private static double _completePercentage = 0;
         
         internal KernelPanic(WinttStatus message, HALException exception)
         {
@@ -25,68 +20,106 @@ namespace WinttOS.Core.Utils.Kernel
         }
         private static void panic(WinttStatus message, object sender)
         {
-            if (FullScreenCanvas.IsInUse)
-                FullScreenCanvas.Disable();
-            _canvas = FullScreenCanvas.GetFullScreenCanvas(new(1024, 768, ColorDepth.ColorDepth32)); // 1024, 768 | 640, 480
+            wSystem.WinttOS.KernelPrint = true;
 
-            _status = message;
+            ShellUtils.PrintTaskResult("Fatal", ShellTaskResult.FAILED, "Kernel panic!");
 
-            updateText();
-            
-            List<string> textToWrite = new()
+            foreach (var p in wSystem.WinttOS.ProcessManager.Processes)
             {
-                "CALL_STACK:",
-                WinttCallStack.GetCallStack(),
-                "LAST_MESSAGES:"
-            };
+                ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.DOING, p.ProcessName);
+                ShellUtils.MoveCursorUp();
 
-            _completePercentage = 0.5;
-            updateText();
+                try
+                {
+                    p.Stop();
 
-            textToWrite.AddRange(WinttDebugger.ErrorMessages);
+                    ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.OK, p.ProcessName);
+                }
+                catch
+                {
+                    ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.FAILED, p.ProcessName);
+                }
+            }
 
-            _completePercentage = 0.9;
-            updateText();
+            int i = 0;
+            foreach (var c in CoroutinePool.Main.RunningCoroutines)
+            {
+                ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.DOING, "Thread" + i);
+                ShellUtils.MoveCursorUp();
 
-            File.WriteAllText(@"0:\core_dump.log", string.Join('\n', textToWrite.ToArray()));
+                try
+                {
+                    c.Stop();
+                    ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.OK, "Thread" + i);
 
-            _completePercentage = 1;
-            updateText();
+                }
+                catch
+                {
+                    ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.FAILED, "Thread" + i);
+                }
 
-            Cosmos.HAL.Global.PIT.Wait(1000);
+                i++;
+            }
+
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "kernel panic:");
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, $"Code: {message.Name}; 0x{message.Value:X8}");
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "kernel panic end");
+
+            System.Console.ReadKey(true);
 
             Power.Reboot();
         }
-
-        private static void updateText()
-        {
-            _canvas.Clear(Color.Red);
-            _canvas.DrawString(":(", Files.Fonts.Font18, Color.White, 10, 10);
-            _canvas.DrawString("WinttOS ran into problem and needs to restart.", Files.Fonts.Font18, Color.White, 10, 30);
-            _canvas.DrawString("We're just collecting some error info, and then", Files.Fonts.Font18, Color.White, 10, 55);
-            _canvas.DrawString("we'll restart for you.", Files.Fonts.Font18, Color.White, 10, 80);
-            _canvas.DrawString($"{_completePercentage:P0} complete", Files.Fonts.Font18, Color.White, 10, 110);
-            _canvas.DrawString($"Stop code: {_status.Name} (0x{_status.Value:X8})", Files.Fonts.Font18, Color.White, 10, 150);
-            _canvas.Display();
-        }
         private static void panic(WinttStatus message, HALException exception)
         {
-            if (FullScreenCanvas.IsInUse)
-                FullScreenCanvas.Disable();
-            _canvas = FullScreenCanvas.GetFullScreenCanvas(new(1024, 768, ColorDepth.ColorDepth32)); // 1024, 768 | 640, 480
 
-            _canvas.Clear(Color.Black);
-            _canvas.DrawString($"CPU Exception x{exception.CTXInterrupt} occurred in WinttOS", Files.Fonts.Font18, Color.White, 10, 10);
-            _canvas.DrawString($"Exception: {exception.Exception}", Files.Fonts.Font18, Color.White, 10, 30);
-            _canvas.DrawString($"Description: {exception.Description}", Files.Fonts.Font18, Color.White, 10, 55);
-            _canvas.DrawString($"Wintt version: {wSystem.WinttOS.WinttVersion}", Files.Fonts.Font18, Color.White, 10, 80);
-            _canvas.DrawString($"Wintt revision: ", Files.Fonts.Font18, Color.White, 10, 110);
-            if(exception.LastKnownAddress != "")
+            wSystem.WinttOS.KernelPrint = true;
+
+            ShellUtils.PrintTaskResult("Fatal", ShellTaskResult.FAILED, "Kernel panic!");
+
+            foreach (var p in wSystem.WinttOS.ProcessManager.Processes)
             {
-                _canvas.DrawString($"Last known address: 0x{exception.LastKnownAddress}", Files.Fonts.Font18, Color.White, 10, 125);
+                ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.DOING, p.ProcessName);
+                ShellUtils.MoveCursorUp();
+
+                try
+                {
+                    p.Stop();
+
+                    ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.OK, p.ProcessName);
+                }
+                catch
+                {
+                    ShellUtils.PrintTaskResult("Stopping", ShellTaskResult.FAILED, p.ProcessName);
+                }
             }
-            _canvas.DrawString($"Press any key to reboot...", Files.Fonts.Font18, Color.White, 10, 175);
-            _canvas.Display();
+
+            int i = 0;
+            foreach (var c in CoroutinePool.Main.RunningCoroutines)
+            {
+                ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.DOING, "Thread" + i);
+                ShellUtils.MoveCursorUp();
+
+                try
+                {
+                    c.Stop();
+                    ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.OK, "Thread" + i);
+
+                }
+                catch
+                {
+                    ShellUtils.PrintTaskResult("Aborting", ShellTaskResult.FAILED, "Thread" + i);
+                }
+
+                i++;
+            }
+
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "kernel panic: ");
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "CPU Exception: x" + exception.CTXInterrupt);
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "Description: " + exception.Description);
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "Wintt version: " + wSystem.WinttOS.WinttVersion);
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "Wintt revision: " + wSystem.WinttOS.WinttRevision);
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "Last knows address: " + (exception.LastKnownAddress == "" ? "knowown" : ("0x" + exception.LastKnownAddress)));
+            ShellUtils.PrintTaskResult("Panic", ShellTaskResult.NONE, "kernal panic end");
 
             System.Console.ReadKey(true);
 
--- WinttOS/wSystem/Properies/VersionInfo.cs
+++ WinttOS/wSystem/Properies/VersionInfo.cs
@@ -2,6 +2,6 @@ namespace WinttOS.wSystem
 {
     public sealed class VersionInfo
     {
-        public static readonly string revision = "202410051030";
+        public static readonly string revision = "202410051104";
     }
 }
