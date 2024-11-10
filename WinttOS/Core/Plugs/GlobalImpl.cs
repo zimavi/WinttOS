1038cdc6dd40e9667cdba7f534d3df70198a426d
zimavi <dev@zimavi.net.ua>
2024-11-10 14:51:46 +0200

    Place plugs inside winttos iteself so workflow can build (probably)

---
+++ WinttOS/Core/Plugs/GlobalImpl.cs
@@ -0,0 +1,21 @@
+﻿using Cosmos.HAL;
+using IL2CPU.API.Attribs;
+using WinttOS.Core.Utils.Debugging;
+
+namespace WinttOS.Core.Plugs
+{
+    [Plug(Target = typeof(Cosmos.System.Global))]
+    public static class Global
+    {
+        public static void Init(TextScreenBase textScreen, bool initScrollWheel = true, bool initPS2 = true, bool initNetwork = true, bool ideInit = true)
+        {
+            Logger.DoOSLog("[Info] Initializing VGA console");
+            Cosmos.System.Global.Console = new Cosmos.System.Console(textScreen);
+
+            Cosmos.HAL.Global.Init(textScreen, initScrollWheel, initPS2, initNetwork, ideInit);
+
+            Logger.DoOSLog("[Info] Initializing network stack");
+            Cosmos.System.Network.NetworkStack.Initialize();
+        }
+    }
+}
---
+++ WinttOS/Core/Plugs/INTs.cs
@@ -0,0 +1,34 @@
+﻿using IL2CPU.API.Attribs;
+using static Cosmos.Core.INTs;
+
+namespace WinttOS.Core.Plugs
+{
+    [Plug(Target = typeof(Cosmos.Core.INTs))]
+    public class INTs
+    {
+        public static void HandleException(uint aEIP, string aDescription, string aName, ref IRQContext ctx, uint lastKnownAddressValue = 0)
+        {
+            const string xHex = "0123456789ABCDEF";
+
+            string ctxInterrupt = "";
+            ctxInterrupt = ctxInterrupt + xHex[(int)((ctx.Interrupt >> 4) & 0xF)];
+            ctxInterrupt = ctxInterrupt + xHex[(int)(ctx.Interrupt & 0xF)];
+
+            string lastKnownAddress = "";
+
+            if (lastKnownAddressValue != 0)
+            {
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 28) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 24) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 20) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 16) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 12) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 8) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)((lastKnownAddressValue >> 4) & 0xF)];
+                lastKnownAddress = lastKnownAddress + xHex[(int)(lastKnownAddressValue & 0xF)];
+            }
+
+            Kernel.WinttRaiseHardError("Unknown excpetion accured!", new Utils.Kernel.HALException(aName, aDescription, lastKnownAddress, ctxInterrupt));
+        }
+    }
+}
